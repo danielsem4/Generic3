436e5e31a88d69997e0bf56af72344c389a1b81d
Shoham Amira <shoham.a01@gmail.com>
2026-01-15 18:34:59 +0200

    login + verify pages

---
+++ src/screens/login/Login.tsx
@@ -0,0 +1,66 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-background">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto text-sm text-primary hover:text-primary/80 font-medium"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+
+      <CardFooter className="flex-col gap-2">
+      <Button asChild
+       className="w-full">
+       <Link to="/verify">Login</Link>
+       </Button>
+
+      </CardFooter>
+    </Card>
+    </div>
+  )
+}
+
